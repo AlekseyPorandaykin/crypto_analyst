e407171e048985a6a95f39be0a40c0e077d48f00
alexey.porandaikin <porandusy@mail.ru>
2023-12-26 02:05:50 +0200

    Add snapshot

--- cmd/root.go
+++ cmd/root.go
@@ -3,19 +3,85 @@ package cmd
 import (
 	"context"
 	"errors"
+	"fmt"
+	"github.com/AlekseyPorandaykin/crypto_analyst/internal/components/price"
+	"github.com/AlekseyPorandaykin/crypto_analyst/internal/repositories"
+	http_server "github.com/AlekseyPorandaykin/crypto_analyst/internal/server/http"
+	"github.com/AlekseyPorandaykin/crypto_analyst/internal/storage"
+	"github.com/AlekseyPorandaykin/crypto_analyst/internal/storage/cache"
+	"github.com/AlekseyPorandaykin/crypto_loader/api/http/client"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
-var rootCmd = &cobra.Command{Use: "crypto-analyst"}
-var PriceCmd = &cobra.Command{
-	Use:   "price",
-	Short: "Price analyst",
-}
-var ServerCmd = &cobra.Command{Use: "server"}
+var rootCmd = &cobra.Command{
+	Use: "price",
+	Run: func(cmd *cobra.Command, args []string) {
+		const DefaultRecalculateDuration = 5 * time.Minute
+		const DefaultPriceAggregationDuration = 1 * time.Hour
+		ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+		defer cancel()
+		db, err := repositories.CreateDB(repositories.Config{
+			Driver:   "postgres",
+			Username: "crypto_app",
+			Password: "developer",
+			Host:     "localhost",
+			Port:     "5433",
+			Database: "crypto_app",
+		})
+		if err != nil {
+			fmt.Println("Error init database: ", err.Error())
+			return
+		}
+		defer func() { _ = db.Close() }()
+		priceRepo := repositories.NewPriceRepository(db)
+		priceChangesRepo := repositories.NewPriceChanges(db)
+		symbolRepo := repositories.NewSymbols(db)
+		aggregationRepo := repositories.NewAggregation(db)
 
-func init() {
-	rootCmd.AddCommand(PriceCmd, ServerCmd)
+		calculatorApp := price.NewChangeCalculator(priceRepo, priceChangesRepo, symbolRepo)
+
+		priceStorage := storage.NewComposite(cache.NewPrice(), priceRepo)
+
+		loaderApp, err := client.NewClient("http://localhost:8081", http.DefaultClient)
+		if err != nil {
+			fmt.Println("Error init loader: ", err.Error())
+			return
+		}
+		loaderPrice := price.NewLoader(loaderApp, priceStorage)
+		metricCalculator := price.NewMetricCalculator(priceChangesRepo, aggregationRepo, symbolRepo)
+
+		priceController := price.NewController(priceStorage)
+		serv := http_server.NewServer()
+		serv.Registration(priceController)
+
+		go func() {
+			defer cancel()
+			if err := loaderPrice.Run(ctx); err != nil && errors.Is(err, context.Canceled) {
+				fmt.Printf("error execute loader price: %s \n", err.Error())
+			}
+		}()
+		go func() {
+			defer cancel()
+			if err := calculatorApp.Run(ctx, DefaultRecalculateDuration); err != nil {
+				fmt.Printf("error execute app: %s \n", err.Error())
+			}
+		}()
+		go func() {
+			defer cancel()
+			if err := serv.Run(":8082"); err != nil {
+				fmt.Println("error execute server: ", err.Error())
+			}
+		}()
+
+		go metricCalculator.Run(ctx, DefaultPriceAggregationDuration)
+
+		<-ctx.Done()
+	},
 }
 
 func Execute() {
